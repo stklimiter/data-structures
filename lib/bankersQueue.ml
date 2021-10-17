8662b8a7a48217a5fe34812478418124485aa176
ristim <32619521+ristim@users.noreply.github.com>
2021-10-17 14:33:34 +0200

    bankersQueue | added dequeue, tests for dequeue, improved documentation, improved formatting of code.

--- lib/bankersQueue.ml
+++ lib/bankersQueue.ml
@@ -1,12 +1,15 @@
 
-type 'a bankers_queue = {front: 'a list; frontSize : int; rear: 'a list; rearSize: int}
+type 'a bankers_queue   = {front: 'a list; frontSize : int; rear: 'a list; rearSize: int}
 
 
-let from_list l = {front = (List.rev l); frontSize = (List.length l); rear = []; rearSize = 0}
+let from_list l         = {front = (List.rev l); frontSize = (List.length l); rear = []; rearSize = 0}
 
-let check q =       if q.frontSize < q.rearSize
-                    then {front = ( q.front @ (List.rev q.rear) ); frontSize = q.rearSize + q.frontSize; rear = []; rearSize = 0}
-                    else q
+let check q             = if q.frontSize < q.rearSize
+                          then {front = ( q.front @ (List.rev q.rear) ); frontSize = q.rearSize + q.frontSize; rear = []; rearSize = 0}
+                          else q
 
-let enqueue e q = check ({q with rear = [e] @ q.rear; rearSize = q.rearSize + 1})
+let enqueue e q         = ({q with rear = [e] @ q.rear; rearSize = q.rearSize + 1}) |> check
 
+let dequeue q           = match q.front with
+                            | [] -> (None ,q)
+                            | head::tail -> ( Some (head), {q with front = tail; frontSize = q.frontSize - 1} |> check)
--- lib/bankersQueue.mli
+++ lib/bankersQueue.mli
@@ -4,13 +4,16 @@
     Two lazy sinlgy linked list, reverse rear and append to the front if front is smaller than the rear list*)
 type 'a bankers_queue = {front: 'a list; frontSize : int; rear: 'a list; rearSize: int}
 
-(** Converts any list of type 'a, into a bankersQueue of type 'a*)
+(** [from_list lst] Converts any list into a bankers_queue containing the same elements in the same order *)
 val from_list: 'a list -> 'a bankers_queue
 
-(** check [bankers_queue] checks if the back of the bankers_queue is bigger, if so, reverse and append to the front. If not, return queue *)
+(** [check bankers_queue] checks if the rear of the bankers_queue is bigger, if so, reverse and append to the front of a
+new bankers_queue. If not, return bankers_queue *)
 val check: 'a bankers_queue -> 'a bankers_queue
 
-(** enqueue ['a bankers_queue] adds an 'a to the back of the queue *)
+(** [enqueue 'a bankers_queue] adds 'a to the back of the queue *)
 val enqueue: 'a -> 'a bankers_queue -> 'a bankers_queue
 
-(*val empty: 'a bankers_queue*)
+(* [dequeue bankers_queue] tries to return the first value in the stack. Returns a (Maybe, queue), where the
+ queue is inserted bankers_queue, but without the first element (if any) *)
+val dequeue: 'a bankers_queue  -> ('a option * 'a bankers_queue)
